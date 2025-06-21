6156043060d775e62c11526f91bc7c05871aa25f
Alexander Sinelnik <ssinelnik@bk.ru>
2025-06-21 13:56:59 +0500

    add handler

---
+++ handler.rb
@@ -0,0 +1,11 @@
+require_relative 'middleware/runtime'
+require_relative 'middleware/logger'
+require_relative 'app'
+
+app = Rack::Builder.new do
+  use Runtime
+  use AppLogger
+  run TimeApp.new
+end
+
+Rack::Handler::WEBrick.run app
